30c98e192c46a98871e3fee5eee456c8a76ed79b
Jingni Huang <116597109+JNH2@users.noreply.github.com>
2026-03-05 00:41:43 +0000

    feat: completed service registry and product service registration with Eureka

---
+++ service-registry/src/test/java/com/codingdance/service_registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingdance.service_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
